7a5e83cc4dff2b1001303649cc3bfdd992ae60f2
Zack Jackson <40549249+zack-jack@users.noreply.github.com>
2022-08-05 19:54:47 -0400

    add google fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      body: ["Poppins", "sans-serif"],
+      display: ["Poppins", "sans-serif"],
+      sans: ["Poppins", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
