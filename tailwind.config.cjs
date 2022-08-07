5ea775ead69d3dd90bd38a979cb6e01cf6bb357b
Zack Jackson <40549249+zack-jack@users.noreply.github.com>
2022-08-07 11:40:57 -0400

    add desktop header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,33 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "rgb(var(--white) / <alpha-value>)",
+      black: "rgb(var(--black) / <alpha-value>)",
+      primary: "rgb(var(--primary) / <alpha-value>)",
+      secondary: "rgb(var(--secondary) / <alpha-value>)",
+      gray: {
+        100: "rgb(var(--gray-100) / <alpha-value>)",
+        200: "rgb(var(--gray-200) / <alpha-value>)",
+        300: "rgb(var(--gray-300) / <alpha-value>)",
+        400: "rgb(var(--gray-400) / <alpha-value>)",
+        500: "rgb(var(--gray-500) / <alpha-value>)",
+        700: "rgb(var(--gray-700) / <alpha-value>)",
+        800: "rgb(var(--gray-800) / <alpha-value>)",
+        900: "rgb(var(--gray-900) / <alpha-value>)",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        md: "4rem",
+        lg: "8rem",
+      },
+    },
     fontFamily: {
       body: ["Poppins", "sans-serif"],
       display: ["Poppins", "sans-serif"],
